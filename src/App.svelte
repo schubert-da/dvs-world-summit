4d41042507858d6f768c42b11f2565ec87f59c92
schubert <schubertdeabreu2@gmail.com>
2023-02-03 11:36:05 +0530

    added sources

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,9 @@
   import PovertySection from "./components/sections/poverty/poverty-section.svelte";
   import HungerSection from "./components/sections/hunger/hunger-section.svelte";
   import HealthSection from "./components/sections/health/health-section.svelte";
-
   import HungerPovertyTable from "./components/sections/common/charts/hunger-poverty-table.svelte";
+  import Sources from "./components/sources/sources.svelte";
+
 </script>
 
   <header>
@@ -32,11 +33,12 @@
 
   <HealthSection />
 
+  <Sources />
+
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,600;0,700&family=Hahmlet:ital,wght@0,400;0,700&family=Glass+Antiqua:ital,wght@0,400&display=swap");
 
   header{
-    width: 100%;
     padding: 20px 20px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   }
---
+++ src/components/sources/sources.svelte
@@ -0,0 +1,130 @@
+<script>
+</script>
+
+<div class="sources-section section">
+    <hr />
+    <h2>Sources</h2>
+    <ul>
+        <li>
+            <a href="https://github.com/owid/poverty-data">Data on Poverty by Our World in Data</a>
+            <br>
+            <p>
+                A global dataset of poverty and inequality measures prepared by Our
+                World in Data from the World Bank's Poverty and Inequality Platform (PIP)
+                database.
+            </p>
+        </li>
+
+        <li>
+            <a href="https://www.un.org/development/desa/dspd/wp-content/uploads/sites/22/2021/05/Mahler_Paper.pdf">
+                Nowcast of extreme poverty, 2015-2021
+            </a>
+
+            <br>
+            <p>
+                <a href="https://documents1.worldbank.org/curated/en/765601591733806023/pdf/How-Much-Does-Reducing-Inequality-Matter-for-Global-Poverty.pdf">
+                    Lakner et al (2020)(updated) </a>, 
+                <a href="http://iresearch.worldbank.org/PovcalNet/home.aspx">PovcalNet</a>, 
+                <a href="https://www.worldbank.org/en/publication/global-economic-prospects">Global Economic Prospects</a>
+            </p>
+        </li>
+
+        <li>
+            <a href="https://dashboards.sdgindex.org/downloads">Online database for the Sustainable Development Report 2022</a>
+            <br>
+            <p>
+                The Sustainable Development Report 2022 marks the seventh edition of the annual assessment of progress of all UN Member States towards the Sustainable Development Goals.
+                database. Used in tables for country-wise SDG rankings and trends.
+            </p>
+        </li>
+
+        <li>
+            <a href="https://databank.worldbank.org/reports.aspx?dsid=2&series=SN.ITK.DEFC.ZS#">The World Bank - World Development Indicators</a>
+            <br>
+            <p>
+                Metrics for percentage of population suffering from undernourishment.
+            </p>
+        </li>
+
+        <li>
+            <a href="http://www.fao.org/faostat/en/#data">Food and Agriculture Organization(FAO) of the United Nations</a>
+            <br>
+            <p>
+                Number of people that were defined as severely food insecure according to region.
+            </p>
+        </li>
+
+        <li>
+            <a href="https://vizhub.healthdata.org/gbd-results/">Global Burden of Disease Collaborative Network</a>
+            <br>
+            <p>
+                Data on global deaths due to Malaria, Tuberculosis.
+            </p>
+        </li>
+
+        <li>
+            <a href="https://data.worldbank.org/indicator/SH.MMR.DTHS?view=chart">The World Bank - Number of maternal deaths</a>
+        </li>
+
+        <li>
+            <a href="http://www.fao.org/faostat/en/#data">WHO (via Our World in Data)</a>
+            <p>
+                Data on global reported cases of Polio.
+            </p>
+        </li>
+
+        <li>
+            <a href="https://www.who.int/teams/global-malaria-programme/elimination/countries-and-territories-certified-malaria-free-by-who">WHO</a>
+            <p>
+                Data on the number of countries that have eliminated Malaria.
+            </p>
+        </li>
+
+        <li>
+            <a href="https://ourworldindata.org/smoking">Our World in Data - Smoking</a>
+        </li>
+
+        <li>
+            <a href="https://ourworldindata.org/hiv-aids">Our World in Data - HIV / AIDS</a>
+        </li>   
+
+        <li>
+            <a href="https://data.worldbank.org/indicator/SP.DYN.IMRT.IN">The World Bank</a>
+            <p>
+                Global infant mortality rates.
+            </p>
+        </li>
+
+        <li>
+            <a href="https://population.un.org/wpp/Download/Standard/MostUsed/">Unite Nations - World Population Prospects</a>
+            <p>
+                Global data on life expectancy in different countries.
+            </p>
+        </li>
+    </ul>
+</div>
+
+<style>
+    .sources-section.section{
+        width: fit-content;
+        padding-bottom: 20px;
+        margin-top: 50px;
+    }
+
+    a {
+        color: #444;
+        font-weight: 600;
+    }
+
+    p {
+        margin: 4px 0;
+        max-width: 70ch;
+        color: #444;
+        font-size: 16px;
+        line-height: 1.3;
+    }
+
+    li{
+        margin-bottom: 20px;
+    }
+</style>
